859d7e883e380cd61dc29df25f39ee6e0d67f153
Muhammad Ammar Raza <ammar.raza32002@gmail.com>
2025-10-07 16:03:38 +0500

    fixed error in _test.py

--- _test.py
+++ _test.py
@@ -9,15 +9,15 @@ def cube(n):
 def fifth(n):
     return n**5
 
-def test_square(n):
+def test_square():
     assert square(2) == 4, 'Test Failed: square of 2 should be 4'
     assert square(3) == 9, 'Test Failed: square of 3 should be 9'
 
-def test_cube(n):
+def test_cube():
     assert cube(2) == 8, 'Test Failed: cube of 2 should be 8'
     assert cube(3) == 27, 'Test Failed: cube of 3 should be 27'
 
-def test_fifth(n):
+def test_fifth():
     assert fifth(2) == 32, 'Test Failed: fifth of 2 should be 32'
     assert fifth(3) == 243, 'Test Failed: fifth of 3 should be 243'
 
